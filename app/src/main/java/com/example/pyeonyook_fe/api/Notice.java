aaa70ba539a7892baeadc1ea5b4002912ef61add
robin <robinsky2002@naver.com>
2025-06-05 17:29:24 +0900

    feat

--- app/src/main/java/com/example/pyeonyook_fe/api/Notice.java
+++ app/src/main/java/com/example/pyeonyook_fe/api/Notice.java
@@ -2,7 +2,7 @@ package com.example.pyeonyook_fe.api;
 
 public class Notice {
     private int id;
-    private int type;   // "학사" 등
+    private int notice_type;   // "학사" 등
     private String title;
     private String author;
     private String url;
@@ -12,7 +12,7 @@ public class Notice {
 
     public Notice(int id, int type, String title, String author, String publishedAt) {
         this.id = id;
-        this.type = type;
+        this.notice_type = type;
         this.title = title;
         this.author = author;
         this.publishedAt = publishedAt;
@@ -21,8 +21,8 @@ public class Notice {
     // getter/setter 모두 추가할 것!
     public int getId() { return id; }
     public void setId(int id) { this.id = id; }
-    public int getType() { return type; }
-    public void setType(int type) { this.type = type; }
+    public int getNotice_type() { return notice_type; }
+    public void setNotice_type(int notice_type) { this.notice_type = notice_type; }
     public String getTitle() { return title; }
     public void setTitle(String title) { this.title = title; }
     public String getAuthor() { return author; }
